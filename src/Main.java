d9070c2c062ab46a6dd112d17d33ca5a9c9d3f20
Dm&try <dmitrypanyukov@gmail.com>
2024-05-25 13:46:12 +0300

    ДЗ выполнена. Поправил.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println();
     }
 
-    public static void determinantOfHighness(int year) {
+    public static void determineLeapYear(int year) {
         int diffYear = year - 1584;
         int remains = diffYear % 4;
         if (year >= 1584 && year % 400 == 0) {
@@ -29,9 +29,9 @@ public class Main {
         } else if (yearOfRelease <currentYear && operatingSystem == 0) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
-        if (yearOfRelease >= currentYear && operatingSystem == 1) {
+        if (yearOfRelease == currentYear && operatingSystem == 1) {
             System.out.println("Установите обычную версию приложения для Android по ссылке");
-        } else if (yearOfRelease >= currentYear && operatingSystem == 0) {
+        } else if (yearOfRelease == currentYear && operatingSystem == 0) {
             System.out.println("Установите обычную версию приложения для iOS по ссылке");
         }
     }
@@ -39,11 +39,11 @@ public class Main {
     public static void deliveryTimeOfCard  (int deliveryDistance) {
         int time = 1;
         if (deliveryDistance <= 20) {
-            System.out.println("Карта будет доставленна вам в течении " +time+ " суток");
+            System.out.println("Карта будет доставлена вам в течении " +time+ " суток");
         } else if (deliveryDistance > 20 && 60 >= deliveryDistance) {time += 1;
-            System.out.println("Карта будет доставленна вам в течении " +time+ " суток");
+            System.out.println("Карта будет доставлена вам в течении " +time+ " суток");
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {time += 1;
-            System.out.println("Карта будет доставленна вам в течении " +time+ " суток");
+            System.out.println("Карта будет доставлена вам в течении " +time+ " суток");
         } else if (deliveryDistance > 100) {
             System.out.println("Доставка на растояние " +deliveryDistance+ " невыполняется");
         }
@@ -56,14 +56,18 @@ public class Main {
         dZ(dZ);
 
         int year1 = 2024;
-        determinantOfHighness(year1);
+        determineLeapYear(year1);
 
         ++dZ;
         dZ(dZ);
 
         int clientOS = 1;
-        int clientDeviceYear = 2025;
-        identificationOfApplicationForOS(clientOS, clientDeviceYear);
+        int clientDeviceYear = 2023;
+        if ((clientOS != 0 && clientOS != 1) || clientDeviceYear > LocalDate.now().getYear()) {
+            System.out.println("Для данной операционной системы приложение отсутствуе или введён не возможный год выпуска устройства");
+        } else {
+            identificationOfApplicationForOS(clientOS, clientDeviceYear);
+        }
 
         ++dZ;
         dZ(dZ);
